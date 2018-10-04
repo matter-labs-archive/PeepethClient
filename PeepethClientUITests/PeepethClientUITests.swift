6f2150baabc60878608fbb0880caeae4c3e00a6f
Georgii Fesenko <fesenko.g@gmail.com>
2018-10-04 11:28:21 +0300

    fastlane configured

--- PeepethClientUITests/PeepethClientUITests.swift
+++ PeepethClientUITests/PeepethClientUITests.swift
@@ -30,7 +30,35 @@ class PeepethClientUITests: XCTestCase {
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
-        snapshot("First Screen")
+//        app.buttons["Import Wallet"].tap()
+//
+//        let importWalletButton = app.buttons["Import Wallet"]
+//        importWalletButton.tap()
+//        let pass = app.secureTextFields["Enter password: MIN 5 chars"]
+//        pass.tap()
+//        pass.typeText("123456")
+//
+//        let repeatPass = app.secureTextFields["Repeat Password: MIN 5 chars"]
+//        repeatPass.tap()
+//        repeatPass.typeText("123456")
+//
+//        let textField = app.secureTextFields["Private Key"]
+//        textField.tap()
+//        textField.typeText("PRIVATEKEY")
+//
+//        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
+//        let newButton = app.buttons["Import Wallet"]
+//        newButton.tap()
+        
+        snapshot("Personal Peeps")
+        
+        let tabBarsQuery = XCUIApplication().tabBars
+        tabBarsQuery.children(matching: .button).element(boundBy: 1).tap()
+        sleep(10)
+        snapshot("All the peeps")
+        tabBarsQuery.children(matching: .button).element(boundBy: 2).tap()
+        snapshot("Settings")
+        
     }
 
 }
